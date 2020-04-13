a98e99c886fb78db9dd652ac024542a8220c9a86
Matt <mkaufman87@gmail.com>
2020-04-13 12:46:05 -0700

    Update init.pp

--- site/minecraft/manifests/init.pp
+++ site/minecraft/manifests/init.pp
@@ -17,5 +17,8 @@ class minecraft {}
       ensure => file, 
       source => 'puppet:///modules/minecraft/minecraft.service',
      }
-     
+     service {'minecraft':
+      ensure => running,
+      enable => true,
+      }
 }
